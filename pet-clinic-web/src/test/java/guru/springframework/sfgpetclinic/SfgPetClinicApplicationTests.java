70480a0a0d06cd16e2984125d239c750093d396b
albyrea-git <Albertino#1>
2020-05-08 11:25:34 +0200

    Repackaging with Maven Multi-Module. Closes #1.

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,17 @@
 package guru.springframework.sfgpetclinic;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
+
+
+
