f4d12452888f6adcce8e857ac91fdf24588c8871
albyrea-git <Albertino#1>
2020-05-10 20:00:03 +0200

    add index page and its controller. Closes #8 #12

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package controllers;
+package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String index() {
+    @RequestMapping("/")
+    public String index(){
         return "index";
     }
-
 }
