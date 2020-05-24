f28174dfcfd808340d288b27ebda51f69e792234
albyrea-git <Albertino#1>
2020-05-24 10:41:13 +0200

    added owener contact info and pets + DataLoader. Closes #31

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -9,6 +10,8 @@ import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -35,17 +38,35 @@ public class DataLoader implements CommandLineRunner {
         dog.setName("cat");
         PetType savedCatType = petTypeService.save(cat);
 
-
-
         Owner owner1 = new Owner();
         owner1.setFirstName("Valentina");
         owner1.setLastName("Amadio");
+        owner1.setAddress("via Abbà");
+        owner1.setCity("Tavazzano");
+        owner1.setTelephone("1234567");
         ownerService.save(owner1);
+
+        Pet valePet = new Pet();
+        valePet.setPetType(savedDogType);
+        valePet.setOwner(owner1);
+        valePet.setBirthDate(LocalDate.now());
+        valePet.setName("Axel");
+        owner1.getPets().add(valePet);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Martino");
         owner2.setLastName("Albanese");
+        owner2.setAddress("via del Lisone");
+        owner2.setCity("Carpiano");
+        owner2.setTelephone("99887766");
         ownerService.save(owner2);
+
+        Pet aironPet = new Pet();
+        aironPet.setPetType(savedDogType);
+        aironPet.setOwner(owner2);
+        aironPet.setBirthDate(LocalDate.now());
+        aironPet.setName("Airon");
+        owner2.getPets().add(aironPet);
 
         System.out.println("Owners loaded.....");
 
