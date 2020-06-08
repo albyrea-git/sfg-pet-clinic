c7ec1991d312c2fcde1893072aaabbc3ba005806
albyrea-git <Albertino#1>
2020-06-08 18:21:55 +0200

    JPA to Base Entity. Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GEN_SEQ")
+    @SequenceGenerator(sequenceName = "generic_seq", allocationSize = 1, name = "GEN_SEQ")
     private Long id;
 
     public Long getId() {
