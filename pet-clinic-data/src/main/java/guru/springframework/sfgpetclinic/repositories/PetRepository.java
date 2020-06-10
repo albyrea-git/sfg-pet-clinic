e2c174d74a789ef01d8c6ebc495219c9c72c7834
albyrea-git <Albertino#1>
2020-06-10 19:09:32 +0200

    Added Spring Data JPA Repositories. Closes #37

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetRepository extends CrudRepository<PetRepository, Long> {
+
+}
