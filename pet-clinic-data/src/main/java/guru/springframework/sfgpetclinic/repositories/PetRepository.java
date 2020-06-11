257f9d6c494dc50f72171ec0c0f24393d06d1da7
albyrea-git <Albertino#1>
2020-06-11 10:24:13 +0200

    Added Spring Data JPA Owner Service. Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgpetclinic.repositories;
 
+import guru.springframework.sfgpetclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetRepository extends CrudRepository<PetRepository, Long> {
+public interface PetRepository extends CrudRepository<Pet, Long> {
 
 }
