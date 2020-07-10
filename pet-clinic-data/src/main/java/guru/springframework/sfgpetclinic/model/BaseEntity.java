9c0b644e3181ae2b6b326a56338b0e62bec88103
albyrea-git <Albertino#1>
2020-07-10 14:06:33 +0200

    added Lombok. Closes #49.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
+@Getter
+@Setter
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
@@ -12,11 +17,4 @@ public class BaseEntity implements Serializable {
     //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
